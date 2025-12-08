123cb5c10037fe57ee4680c1457b5927c92e7867
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-09 00:24:30 +0700

    Delete app/api/auth/[...nextauth] directorates

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "@/app/util/authOptions";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
