f071684ef8622c391979a32e89bca45b1207921f
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-09 01:41:33 +0700

    Create index.ts

---
+++ app/components/constant/index.ts
@@ -0,0 +1,30 @@
+import { ReyyTiktok, ReyyFacebookF, ReyyLinkedinIn, ReyyGithub } from "react-icons/Reyy";
+import { ReyyFillInstagram } from "react-icons/Reyy";
+
+export const footerSocials = [
+  {
+    name: "facebook",
+    logo: ReyyFacebookF,
+    url: "https://www.facebook.com/share/1Bg7KnV8X3/",
+  },
+  {
+    name: "instagram",
+    logo: ReyyInstagram,
+    url: "https://www.instagram.com/ryosukexus?igsh=MTQ5enIxb3BwYmozaA==",
+  },
+  {
+    name: "tiktok",
+    logo: ReyyTiktok,
+    url: "https://www.tiktok.com/@yuuzhouzhi?_r=1&_t=ZS-923bwVrHGj4",
+  },
+  {
+    name: "linkedin",
+    logo: ReyyLinkedinIn,
+    url: "https://kr.linkedin.com/in/rey-shoozi-6455783a0",
+  },
+  {
+    name: "github",
+    logo: ReyyGithub,
+    url: "https://github.com/RyyCrasherV1",
+  },
+];
