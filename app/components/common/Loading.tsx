3c36c95f2470e388371f98df57a11fc712e7ae56
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-09 00:47:30 +0700

    Create Loading.tsx

---
+++ app/components/common/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="loading loading-spinner loading-md"></span>
+    </div>
+  );
+}
