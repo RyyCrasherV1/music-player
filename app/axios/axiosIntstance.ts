803c8151f714de9644ccabd34bfba0ad548760ae
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-09 10:58:02 +0700

    Update axiosIntstance.ts

--- app/axios/axiosIntstance.ts
+++ app/axios/axiosIntstance.ts
@@ -1,11 +1,19 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.spotify.com/v1",
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: "https://api.spotify.com/v1",
+    timeout: 5000,
+});
+
+// Inject token secara otomatis
+axiosInstance.interceptors.request.use(async (config) => {
+    const tokenResponse = await fetch("/api/token");
+    const { access_token } = await tokenResponse.json();
+
+    config.headers.Authorization = `Bearer ${access_token}`;
+    config.headers["Content-Type"] = "application/json";
+
+    return config;
 });
 
 export default axiosInstance;
