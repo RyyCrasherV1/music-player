88f920cf67a415b4df32614369f9063e3da9d079
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-09 11:22:14 +0700

    Update index.ts

--- app/constant/index.ts
+++ app/constant/index.ts
@@ -1,30 +1,30 @@
-import { ReyyTiktok, ReyyFacebookF, ReyyLinkedinIn, ReyyGithub } from "react-icons/Reyy";
-import { ReyyFillInstagram } from "react-icons/Reyy";
+import { FaFacebookF, FaTiktok, FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 export const footerSocials = [
   {
     name: "facebook",
-    logo: ReyyFacebookF,
+    logo: FaFacebookF,
     url: "https://www.facebook.com/share/1Bg7KnV8X3/",
   },
   {
     name: "instagram",
-    logo: ReyyInstagram,
-    url: "https://www.instagram.com/ryosukexus?igsh=MTQ5enIxb3BwYmozaA==",
+    logo: AiOutlineInstagram,
+    url: "https://www.instagram.com/ryosukexus?igsh=MTQ5enIxb3BWYmozaA==",
   },
   {
     name: "tiktok",
-    logo: ReyyTiktok,
+    logo: FaTiktok,
     url: "https://www.tiktok.com/@yuuzhouzhi?_r=1&_t=ZS-923bwVrHGj4",
   },
   {
     name: "linkedin",
-    logo: ReyyLinkedinIn,
+    logo: FaLinkedinIn,
     url: "https://kr.linkedin.com/in/rey-shoozi-6455783a0",
   },
   {
     name: "github",
-    logo: ReyyGithub,
+    logo: FaGithub,
     url: "https://github.com/RyyCrasherV1",
   },
 ];
