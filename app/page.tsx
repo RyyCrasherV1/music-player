20fcecbf77171f6ae898c11c3dede570ccb12ea1
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-25 02:35:52 +0700

    Refactor Home component and update layout
    Refactored Home component to HomePage and updated layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,23 @@
-"use client";
-import Image from "next/image";
-import { FaHeadphones } from "react-icons/fa";
-import Footer from "./components/layout/Footer";
-import { signIn } from "next-auth/react";
+import LoginButton from "./components/LoginButton";
+import SavedTracks from "./components/SavedTracks";
+import TopTracks from "./components/TopTracks";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="min-h-screen bg-dark">
-      <div className="flex min-h-screen items-center justify-center">
-        <div className="flex items-center p-2">
-          <Image
-            src="/logo.png"
-            width={500}
-            height={500}
-            alt="Logo"
-            className="hidden md:block"
-            priority
-          />
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">My Spotify Dashboard</h1>
 
-          <div className="flex max-w-md flex-col items-center gap-5 text-center text-white">
-            <FaHeadphones size={30} />
-            <h1 className="text-2xl font-bold">
-              Stream your favorite music on{" "}
-              <span className="block text-primary">Himig Play.</span>
-            </h1>
+      <LoginButton />
 
-            <p className="text-sm">
-              Himig Play is a personalized music streaming app powered by{" "}
-              <span className="text-primary">Spotify API</span>, offering a
-              seamless experience to discover, play, and enjoy your favorite
-              tunes
-            </p>
-            <button
-              onClick={() => signIn()}
-              className="btn btn-ghost inline-flex items-center gap-2 rounded-md border-2 border-gray-700 p-2"
-            >
-              <Image
-                src="/spotify-icon.svg"
-                width={20}
-                height={20}
-                alt="Spotify Logo"
-              />
-              Login with Spotify.
-            </button>
-          </div>
-        </div>
-      </div>
-      <Footer />
+      <section className="mt-8">
+        <h2 className="text-xl font-semibold mb-2">Top Tracks</h2>
+        <TopTracks />
+      </section>
+
+      <section className="mt-8">
+        <h2 className="text-xl font-semibold mb-2">Saved Tracks</h2>
+        <SavedTracks />
+      </section>
     </main>
   );
 }
