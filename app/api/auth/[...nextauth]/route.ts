9f42f3aeca751625ec8548a034174736df134d86
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-25 02:27:04 +0700

    Refactor NextAuth to use Spotify provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,15 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "@/app/util/authOptions";
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
 
-const handler = NextAuth(authOptions);
+export const handler = NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: "https://accounts.spotify.com/authorize?scope=user-library-read,user-top-read",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
 export { handler as GET, handler as POST };
