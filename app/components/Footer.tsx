88e2d1cbeacd087011360a263a7e58d780caf5c1
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-25 02:39:35 +0700

    Add Footer component with copyright notice

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 bg-gray-900 text-white text-center mt-8">
+      &copy; 2025 Himig Play. All rights reserved.
+    </footer>
+  );
+}
