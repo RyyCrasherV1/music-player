2cd18b9e15f51329285e1ff8f643e34d47066ff5
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-25 02:28:12 +0700

    Refactor getAuthenticatedSession to use new imports

--- app/util/auth.ts
+++ app/util/auth.ts
@@ -1,12 +1,7 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./authOptions";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function getAuthenticatedSession() {
   const session = await getServerSession(authOptions);
-
-  if (!session || !session.token || !session.token.access_token) {
-    throw new Error("Not authenticated");
-  }
-
-  return session.token.access_token;
+  return session?.accessToken || null;
 }
