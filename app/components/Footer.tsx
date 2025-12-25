fc29bcdad87040437aa5ffdebc946fb00da39dbf
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-25 12:16:56 +0700

    Update footer text to remove slash in 'Rey/Zhou'

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-black bg-opacity-50 p-4 text-center text-white">
-      <p>© 2024 Spotify Dashboard. Made with Rey/Zhou</p>
+      <p>© 2024 Spotify Dashboard. Made with ReyZhou</p>
     </footer>
   );
 }
